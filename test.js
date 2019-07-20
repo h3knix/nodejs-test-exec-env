fb8184951809ff7cce7465d1e7a28a4d01123a13
mike <h3knix@gmail.com>
2019-07-20 10:41:19 -0600

    cleaner

--- test.js
+++ test.js
@@ -15,76 +15,70 @@ function assert_test(fnc){
 (async ()=>{
 
   console.log('++ testing child_process.exec()');
-  await (async ()=>assert_test((resolve,reject)=>{
-      let child = child_process.exec(process.execPath +' sub_process.js',ops,(err,stdout)=>{
-        console.log(stdout);
-      });
-      child.on('close',(code,stdout)=>{
-        console.log(code);
-        if ( code ) reject();
-        else resolve();
-      });
-    })
-  )();
+  await assert_test((resolve,reject)=>{
+    let child = child_process.exec(process.execPath +' sub_process.js',ops,(err,stdout)=>{
+      console.log(stdout);
+    });
+    child.on('close',(code,stdout)=>{
+      console.log(code);
+      if ( code ) reject();
+      else resolve();
+    });
+  });
 
 
   console.log('++ testing child_process.execFile()');
-  await (async ()=>assert_test((resolve,reject)=>{
-      let child = child_process.execFile('./sub_process.js',ops,(err,stdout)=>{
-        console.log(stdout);
-      });
-      child.on('close',(code)=>{
-        console.log(code);
-        if ( code ) reject();
-        else resolve();
-      });
-    })
-  )();
+  await assert_test((resolve,reject)=>{
+    let child = child_process.execFile('./sub_process.js',ops,(err,stdout)=>{
+      console.log(stdout);
+    });
+    child.on('close',(code)=>{
+      console.log(code);
+      if ( code ) reject();
+      else resolve();
+    });
+  });
 
 
   console.log('++ testing child_process.fork()');
-  await (async ()=>assert_test((resolve,reject)=>{
-      let child = child_process.fork('./sub_process.js',ops);
-      child.on('close',(code)=>{
-        console.log(code);
-        if ( code ) reject();
-        else resolve();
-      });
-    })
-  )();
+  await assert_test((resolve,reject)=>{
+    let child = child_process.fork('./sub_process.js',ops);
+    child.on('close',(code)=>{
+      console.log(code);
+      if ( code ) reject();
+      else resolve();
+    });
+  });
 
 
   console.log('++ testing child_process.spawn()');
-  await (async ()=>assert_test((resolve,reject)=>{
-      let child = child_process.spawn(process.execPath,['./sub_process.js'],ops);
-      child.stdout.on('data',(data)=>{
-        console.log(data +'');
-      });
-      child.on('close',(code)=>{
-        console.log(code);
-        if ( code ) reject();
-        else resolve();
-      });
-    })
-  )();
+  await assert_test((resolve,reject)=>{
+    let child = child_process.spawn(process.execPath,['./sub_process.js'],ops);
+    child.stdout.on('data',(data)=>{
+      console.log(data +'');
+    });
+    child.on('close',(code)=>{
+      console.log(code);
+      if ( code ) reject();
+      else resolve();
+    });
+  });
 
 
   console.log('++ testing child_process.execFileSync()');
-  await (async ()=>assert_test((resolve,reject)=>{
-      child_process.execFileSync('./sub_process.js',{ stdio: 'inherit', ...ops });
-    })
-  )();
+  await assert_test((resolve,reject)=>{
+    child_process.execFileSync('./sub_process.js',{ stdio: 'inherit', ...ops });
+  });
 
 
   console.log('++ testing child_process.spawnSync()');
-  await (async ()=>assert_test((resolve,reject)=>{
-      let ret = child_process.spawnSync(process.execPath,['./sub_process.js'],ops);
-      console.log(ret.stdout +'');
-      console.log(ret.status);
-      if ( ret.status ) reject();
-      else resolve();
-    })
-  )();
+  await assert_test((resolve,reject)=>{
+    let ret = child_process.spawnSync(process.execPath,['./sub_process.js'],ops);
+    console.log(ret.stdout +'');
+    console.log(ret.status);
+    if ( ret.status ) reject();
+    else resolve();
+  });
 
 
 })()
